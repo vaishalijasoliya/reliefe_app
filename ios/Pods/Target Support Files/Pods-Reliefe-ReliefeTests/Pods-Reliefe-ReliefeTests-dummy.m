240a647d56434c6d8b924b0aaa62f4120ec8e74b
Hiren Surani <hiren.s@3rddigital.com>
2021-11-02 13:03:21 +0530

    design change

--- ios/Pods/Target Support Files/Pods-Reliefe-ReliefeTests/Pods-Reliefe-ReliefeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Reliefe_ReliefeTests : NSObject
-@end
-@implementation PodsDummy_Pods_Reliefe_ReliefeTests
-@end
--- ios/Pods/Target Support Files/Pods-Reliefe/Pods-Reliefe-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Reliefe : NSObject
-@end
-@implementation PodsDummy_Pods_Reliefe
-@end
